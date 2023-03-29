78ae0535336c3abd00c04e7047c02dc2dda52d94
israel <israel.fernandeztorres7@gmail.com>
2023-03-29 13:22:17 -0600

    Initial Commit

---
+++ Ximena2903/Ximena2903App.swift
@@ -0,0 +1,17 @@
+//
+//  Ximena2903App.swift
+//  Ximena2903
+//
+//  Created by CEDAM 11 on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Ximena2903App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
